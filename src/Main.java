3ff87cd892c74a1c5067249c23275e8951de57e8
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-04 14:25:15 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+// Создаем интерфейс HavingSuperAbility
+interface HavingSuperAbility {
+    void applySuperAbility();
+}
+
+// Создаем абстрактный класс Hero с полями и реализацией интерфейса
+abstract class Hero implements HavingSuperAbility {
+    int health;
+    int damage;
+    String superAbilityType;
+
+    public Hero(int health, int damage, String superAbilityType) {
+        this.health = health;
+        this.damage = damage;
+        this.superAbilityType = superAbilityType;
+    }
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println(getClass().getSimpleName() + " применил суперспособность " + superAbilityType);
+    }
+}
+
+// Создаем классы героев Magic, Medic, Warrior и реализуем метод applySuperAbility()
+class Magic extends Hero {
+    public Magic() {
+        super(100, 30, "MAGIC SPELL");
+    }
+}
+
+class Medic extends Hero {
+    public Medic() {
+        super(80, 20, "HEALING");
+    }
+}
+
+class Warrior extends Hero {
+    public Warrior() {
+        super(120, 40, "CRITICAL DAMAGE");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hero[] heroes = {new Magic(), new Medic(), new Warrior()};
+
+        for (Hero hero : heroes) {
+            hero.applySuperAbility();
+        }
     }
 }
